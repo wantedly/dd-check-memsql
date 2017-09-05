0dc50e0434ba6f418ad46c1c978e6da72aa15cb8
Yuichi Saito <munisystem@gmail.com>
2017-09-05 19:13:10 +0900

    Implement MemSQL connector

--- memsql.py
+++ memsql.py
@@ -1,9 +1,21 @@
 from checks import AgentCheck
+from contextlib import contextmanager
+
+# 3rd party
+from memsql.common import database
 
 class MemSQL(AgentCheck):
+    def __init__(self, name, init_config, agentConfig, instances=None):
+        AgentCheck.__init__(self, name, init_config, agentConfig, instances)
+
     def check(self, instance):
         host, port, user, password = self._get_config(instance)
-        self.gauge('hello.world', 1)
+        with self._connect(host, port, user, password) as db:
+            try:
+                db.execute('SELECT 1;')
+            except Exception as e:
+                self.log.exception("error!")
+                raise e
 
     def _get_config(self, instance):
         self.host = instance.get('host', '')
@@ -12,3 +24,16 @@ class MemSQL(AgentCheck):
         password = instance.get('password', '')
 
         return (self.host, self.port, user, password)
+
+    @contextmanager
+    def _connect(self, host, port, user, password):
+        db = None
+        try:
+            db = database.connect(host=host, port=port, user=user, password=password)
+            self.log.debug("Connected to MemSQL")
+            yield db
+        except Exception:
+            raise
+        finally:
+            if db:
+                db.close()
